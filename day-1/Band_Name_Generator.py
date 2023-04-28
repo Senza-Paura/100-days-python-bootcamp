7799eddbc03554499019f1e63d50944aa5d3a0c4
Senza-Paura <othmantahir01@gmail.com>
2023-04-28 19:24:08 +0100

    day-1

---
+++ day-1/Band_Name_Generator.py
@@ -0,0 +1,5 @@
+print ("Hello Ireland brands")
+city = input (" Enter you city name :\n ")
+pet = input ("Enter your pet name :\n ")
+print ("Your brand should be named : "  +  city  + " " +  pet )
+
